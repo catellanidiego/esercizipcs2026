84d1d17435e74bf683245653aebfc1466fef1cfe
catellanidiego <catellanidiego8@gmail.com>
2026-03-11 20:11:05 +0000

    prova1

---
+++ esercizi1/media.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+/**Per lo sviluppo di questo codice è stato utilizzato il supporto dell'IA (Gemini) per chiarimenti sulla sintassi */
+
+int main(int argc, const char *argv[])
+{
+	if (argc < 2) {
+		std::cout << "Errore: file non passato";
+	}
+	else {
+		std::ifstream ifs(argv[1]);
+		if (ifs.is_open()) {
+			for (int r=1; r<= 3; r++){
+				std::string città = "a";
+				float t_1 = 0;
+				float t_2 = 0;
+				float t_3 = 0;
+				float t_4 = 0;
+				ifs >> città >> t_1 >> t_2 >> t_3 >> t_4;
+				float media = 0;
+				media = (t_1 + t_2 + t_3 + t_4)/4;
+				std::cout << città << "  "<< media << "\n";
+			}
+		}
+		else {
+			std::cout << "Errore nell'apertura del file";
+		}
+	}
+	return 0;
+}
