10221b2fbaf599a7c2cc180fa44cf6fef7d4a0d0
catellanidiego <catellanidiego8@gmail.com>
2026-03-29 13:17:42 +0000

    file aggiornati per gestione numeri negativi

---
+++ esercitazione3/header2.hpp
@@ -0,0 +1,23 @@
+#pragma once
+#include <iostream> 
+template<typename I> requires std::integral<I>
+std::ostream& operator<<(std::ostream& os, const rational<I>& n) {
+	I num2 = n.num();
+	I den2 = n.den();
+	if (den2 == 0 && num2 == 0 ){
+		os << "NaN";
+	};
+	if (den2 == 0 && num2 > 0) {
+		os << "+inf";
+	};
+	if (den2 == 0 && num2 < 0) {
+		os << "-inf";
+	};
+	if (den2 !=0 && den2*num2 > 0) {
+		os << num2 << "/" << den2;
+	};
+	if (den2 !=0 && den2*num2 <0) {
+		os << "-" << abs(num2) << "/" << abs(den2); 
+	};
+	return os;
+};
